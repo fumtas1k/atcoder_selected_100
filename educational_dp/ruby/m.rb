e8be53230a2b5a7e85bc83bd9df02c67690389d7
fumtas1k <fumta01a7@gmail.com>
2023-06-06 23:05:07 +0900

    [improve]累積和にreduceを使用

--- educational_dp/ruby/m.rb
+++ educational_dp/ruby/m.rb
@@ -7,13 +7,10 @@ A = gets.split.map(&:to_i)
 # 貰うDP [i番目までの子][分け合う飴の個数] = 通り数
 dp = Array.new(N + 1) { [0] * (K + 1) }
 dp[0][0] = 1
-csum = [0] * (K + 2)
 
 N.times do |i|
-  (K + 1).times do |j|
-    # dp[i]の累積和を求める
-    csum[j + 1] = csum[j] + dp[i][j]
-  end
+  # dp[i]の累積和を求める
+  csum = dp[i].reduce([0]) { |acc, j| acc << acc[-1] + j }
 
   (K + 1).times do |j|
     dp[i + 1][j] = (csum[j + 1] - csum[[j - A[i], 0].max]) % M
