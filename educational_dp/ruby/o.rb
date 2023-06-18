2475188cf057faa6d2ea37ef7c45de2b01e86b16
fumtas1k <fumta01a7@gmail.com>
2023-06-18 18:48:34 +0900

    [improve]少し高速に

--- educational_dp/ruby/o.rb
+++ educational_dp/ruby/o.rb
@@ -8,15 +8,13 @@ A = Array.new(N) { gets.split.map(&:to_i) }
 dp = Array.new(N + 1) { [0] * (1 << N) }
 dp[0][0] = 1
 
-N.times do |i|
-  (1 << N).times do |j|
-    next if dp[i][j].zero?
-    N.times do |k|
-      next if j >> k & 1 == 1 || A[i][k].zero?
-      dp[i + 1][j + (1 << k)] += dp[i][j]
-      dp[i + 1][j + (1 << k)] %= M
-    end
+(1 << N).times do |j|
+  i = j.to_s(2).count("1")
+  next if dp[i][j].zero?
+  N.times do |k|
+    next if j >> k & 1 == 1 || A[i][k].zero?
+    dp[i + 1][j + (1 << k)] += dp[i][j]
   end
 end
 
-puts dp[-1][-1]
+puts dp[-1][-1] % M
