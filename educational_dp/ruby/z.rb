f63f83674d2270dbefa5670758a1749c77df1dd6
fumtas1k <fumta01a7@gmail.com>
2023-06-21 23:42:42 +0900

    [doc]説明追加

--- educational_dp/ruby/z.rb
+++ educational_dp/ruby/z.rb
@@ -51,13 +51,16 @@ class CHT
 end
 
 N, C = gets.split.map(&:to_i)
+# Hは単調増加
 H = gets.split.map(&:to_i)
 dp = [0] * N
 cht = CHT.new
 
 1.upto(N - 1) do |i|
   # dp[i] = [dp[i], dp[j] + (H[i] - H[j]) ** 2 + C].min　を式展開
-  # H[i] ** 2 + C + (- 2 * H[i]H[j] ** H[j] ** 2)(i - 1までの最小)
+  # H[i] ** 2 + C + (- 2 * H[i]H[j] + H[j] ** 2)(i - 1までの最小)
+  # a = - 2 * H[j], b = H[j] ** 2
+  # Hは単調増加なので、追加するaは常に最小となる
   cht.add(-2 * H[i - 1], dp[i - 1] + H[i - 1] ** 2)
   dp[i] = H[i] ** 2 + C + cht.miny(H[i])
 end
