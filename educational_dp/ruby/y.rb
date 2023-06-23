f0462c8073e7a95b9d8c555f3a4ebe443bf6d63a
fumtas1k <fumta01a7@gmail.com>
2023-06-24 00:11:10 +0900

    [add]y ruby追加

---
+++ educational_dp/ruby/y.rb
@@ -0,0 +1,50 @@
+# Y
+
+class Comb
+  attr_accessor :mod, :fact, :inv_fact
+
+  def initialize(h, r, mod)
+    @mod = mod
+    @fact = [1, 1]
+    @inv_fact = [1, 1]
+    prepare(h + r)
+  end
+
+  def c(n, r)
+    fact[n] * inv_fact[r] * inv_fact[n - r] % mod
+  end
+
+  def prepare(n)
+    return if n <= 1
+    2.upto(n) do |i|
+      fact[i] = i * fact[i - 1] % mod
+      inv_fact[i] = i.pow(mod - 2, mod) * inv_fact[i - 1] % mod
+    end
+  end
+end
+
+M = 10 ** 9 + 7
+H, W, N = gets.split.map(&:to_i)
+# マンハッタン距離でソート. 順番に処理するのに必要
+WALLS = Array.new(N) { gets.split.map(&:to_i).map(&:pred) }.sort_by { _1.reduce(:+) }
+WALLS << [H - 1, W - 1]
+dp = [0] * (N + 1)
+dp[0] = 1
+comb = Comb.new(H, W, M)
+
+# 壁を1つも通らず壁まで辿り着く場合の数 = (壁を1つ以上通って壁にたどり着く場合の数)の余事象
+# 壁の最後をゴールとしているので順番に求めていけばdpの最後が答え
+WALLS.each_with_index do |(r, c), i|
+  # 壁iまで辿り着く場合の数(壁を通る場合を含む)
+  dp[i] = comb.c(r + c, r)
+  i.times do |j|
+    r0, c0 = WALLS[j]
+    r1, c1 = r - r0, c - c0
+    # 壁jから壁iに辿り着けないものを除く
+    next if r1 < 0 || c1 < 0
+    # 壁iにたどり着く場合の数 - 壁jを通って壁jにたどり着く場合の数
+    dp[i] = (dp[i] - dp[j] * comb.c(r1 + c1, r1)) % M
+  end
+end
+
+puts dp[-1]
