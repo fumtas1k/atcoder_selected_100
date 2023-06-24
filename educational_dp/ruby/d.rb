e3349428d50cf98e7e93749dbf4227f846c6fb29
fumtas1k <fumta01a7@gmail.com>
2023-06-24 11:41:29 +0900

    [improve]スマートに

--- educational_dp/ruby/d.rb
+++ educational_dp/ruby/d.rb
@@ -2,14 +2,12 @@
 
 N, W = gets.split.map(&:to_i)
 WV = Array.new(N) { gets.split.map(&:to_i) }
-
 dp = Array.new(N + 1) { [0] * (W + 1) }
 
-N.times do |i|
-  w, v = WV[i]
+WV.each_with_index do |(w, v), i|
   (W + 1).times do |j|
     dp[i + 1][j] = dp[i][j]
-    dp[i + 1][j] = [dp[i + 1][j], dp[i][j - w] + v].max if j - w >= 0
+    dp[i + 1][j] = [dp[i + 1][j], dp[i][j - w] + v].max if j >= w
   end
 end
 
