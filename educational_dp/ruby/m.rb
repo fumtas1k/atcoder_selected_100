2577cce8ee500a4f86c6b2dbbed46fb2e51fe0ca
fumtas1k <fumta01a7@gmail.com>
2023-06-26 23:13:58 +0900

    [improve]スマートに

--- educational_dp/ruby/m.rb
+++ educational_dp/ruby/m.rb
@@ -4,16 +4,16 @@ M = 10 ** 9 + 7
 N, K = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
 
-# 貰うDP [i番目までの子][分け合う飴の個数] = 通り数
+# 貰うDP [i人目までの子][分け合う飴の個数] = 通り数
 dp = Array.new(N + 1) { [0] * (K + 1) }
 dp[0][0] = 1
 
-N.times do |i|
+A.each_with_index do |a, i|
   # dp[i]の累積和を求める
   csum = dp[i].reduce([0]) { |acc, j| acc << acc[-1] + j }
 
   (K + 1).times do |j|
-    dp[i + 1][j] = (csum[j + 1] - csum[[j - A[i], 0].max]) % M
+    dp[i + 1][j] = (csum[j + 1] - csum[[j - a, 0].max]) % M
   end
 end
 
