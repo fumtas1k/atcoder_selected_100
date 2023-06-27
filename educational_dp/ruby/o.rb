66f4101f3cabe53971809fa097247d462cc7cd2c
fumtas1k <fumta01a7@gmail.com>
2023-06-27 23:08:33 +0900

    [change]少し速度アップ

--- educational_dp/ruby/o.rb
+++ educational_dp/ruby/o.rb
@@ -5,6 +5,8 @@ M = 10 ** 9 + 7
 N = gets.to_i
 A = Array.new(N) { gets.split.map(&:to_i) }
 
+# i番目までの男性がペアになっている、 j = 2進数で1となった番号の女性はペアとなっている
+# dp[i][j] = 場合の数
 dp = Array.new(N + 1) { [0] * (1 << N) }
 dp[0][0] = 1
 
@@ -12,7 +14,7 @@ dp[0][0] = 1
   i = j.to_s(2).count("1")
   next if dp[i][j].zero?
   N.times do |k|
-    next if j >> k & 1 == 1 || A[i][k].zero?
+    next if j[k] == 1 || A[i][k].zero?
     dp[i + 1][j + (1 << k)] += dp[i][j]
   end
 end
