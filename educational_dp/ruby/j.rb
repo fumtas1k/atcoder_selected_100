c92e8c43d6f2f3d8072186114e7ea5b1a69ba5ef
fumtas1k <fumta01a7@gmail.com>
2023-08-15 11:13:21 +0900

    [improve]説明追加

--- educational_dp/ruby/j.rb
+++ educational_dp/ruby/j.rb
@@ -1,17 +1,17 @@
 # J
 
 N = gets.to_i
-A = gets.chomp.split.map(&:to_i).tally
+A = gets.split.map(&:to_i).tally
 A.default = 0
 
-three = A[3] + 1
-two = three + A[2]
-one = two + A[1]
-
-DP = Array.new(three) { Array.new(two) { Array.new(one) } }
+# DP[i][j][k] := 3個の皿の数i, 2個の皿の数j, 1個の皿の数k, 0個の皿の数N-i-j-kの時の操作回数の期待値
+# 求める値はDP[A[3]][A[2]][A[1]]
+# DP[i][j][k] = (DP[i][j][k]*(N-i-j-k)/N + DP[i-1][j+1][k]*i/N + DP[i][j-1][k+1]*j/N + DP[i][j][k-1]*k/N) + 1
+# 変形すると
+# DP[i][j][k] = (DP[i-1][j+1][k] + DP[i][j-1][k+1] + DP[i][j][k-1] + N) / (i+j+k)
+DP = Array.new(N + 1) { Array.new(N + 1) { Array.new(N + 1) } }
 DP[0][0][0] = 0.0
 
-# i: 3個の皿の数, j: 2個の皿の数, k: 1個の皿の数
 def dfs(i, j, k)
   return DP[i][j][k] if DP[i][j][k]
   exp = N
