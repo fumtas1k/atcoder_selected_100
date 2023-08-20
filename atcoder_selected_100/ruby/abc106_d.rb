85e4d2818e235ddd468099dcef78d5ef0989432e
fumtas1k <fumta01a7@gmail.com>
2023-08-20 18:44:32 +0900

    [improve]ループを１つに

--- atcoder_selected_100/ruby/abc106_d.rb
+++ atcoder_selected_100/ruby/abc106_d.rb
@@ -12,14 +12,10 @@ LR.each {|l, r| dp[l][r] += 1 }
 
 # dp[l][r]を区間[l, r]に完全に含まれる列車の本数にするため累積和を用いる
 # dp[l][l + 1] + ... + dp[l][r] + dp[l + 1][l + 2] + ... + dp[r - 2][r] + dp[r - 1][r]
-(N + 1).times do |i|
-  N.times do |j|
-    dp[i][j + 1] += dp[i][j]
-  end
-end
-N.downto(1) do |i|
+N.downto(0) do |i|
   (N + 1).times do |j|
-    dp[i - 1][j] += dp[i][j]
+    dp[i][j + 1] += dp[i][j] if j < N
+    dp[i][j] += dp[i + 1][j] if i < N
   end
 end
 
