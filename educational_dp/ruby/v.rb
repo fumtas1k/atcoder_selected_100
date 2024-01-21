9e0aad922334981ce04bbfe17831ed6beb7bdf08
fumtas1k <fumta01a7@gmail.com>
2024-01-21 14:25:59 +0900

    [improve]

--- educational_dp/ruby/v.rb
+++ educational_dp/ruby/v.rb
@@ -1,4 +1,5 @@
 # V
+# 全域木
 
 N, M = gets.split.map(&:to_i)
 G = Array.new(N) { [] }
@@ -8,41 +9,37 @@ G = Array.new(N) { [] }
   G[y] << x
 end
 
-@dp = [0] * N
+@size = []
 # posが黒色でかつ、黒色の頂点が全て連結である組み合わせを求める
 def dfs1(pos, pre = -1)
-  return @dp[pos] unless @dp[pos].zero?
-  @dp[pos] = 1
+  return @size[pos] if @size[pos]
+  @size[pos] = 1
   G[pos].each do |i|
     next if i == pre
     # 1加算するのはpos以外白色だった場合を考慮
-    @dp[pos] *= dfs1(i, pos) + 1
-    @dp[pos] %= M
+    @size[pos] *= dfs1(i, pos) + 1
+    @size[pos] %= M
   end
-  @dp[pos]
+  @size[pos]
 end
 
-@ans = [0] * N
+@ans = []
 def dfs2(pos, pre = -1)
   @ans[pos] = 1
-  G[pos].each do |i|
-    @ans[pos] *= @dp[i] + 1
-    @ans[pos] %= M
-  end
-
-  n = G[pos].size
   # l_cproduct, r_cproduct は累積積
   l_cproduct = []
   r_cproduct = []
   G[pos].each_with_index do |u, i|
-    l_cproduct[i] = r_cproduct[i] = @dp[u] + 1
+    @ans[pos] *= @size[u] + 1
+    @ans[pos] %= M
+    l_cproduct[i] = r_cproduct[i] = (@size[u] + 1) % M
   end
 
+  n = G[pos].size
   1.upto(n - 1) do |i|
     l_cproduct[i] *= l_cproduct[i - 1]
     l_cproduct[i] %= M
   end
-
   (n - 2).downto(0) do |i|
     r_cproduct[i] *= r_cproduct[i + 1]
     r_cproduct[i] %= M
@@ -50,11 +47,11 @@ def dfs2(pos, pre = -1)
 
   G[pos].each_with_index do |u, i|
     next if u == pre
-    # 親がiの場合の@dp[pos]を計算する
-    @dp[pos] = 1
-    @dp[pos] *= l_cproduct[i - 1] unless i.zero?
-    @dp[pos] *= r_cproduct[i + 1] if i + 1 < n
-    @dp[pos] %= M
+    # 親がuの場合の@size[pos]を計算する
+    @size[pos] = 1
+    @size[pos] *= l_cproduct[i - 1] unless i.zero?
+    @size[pos] *= r_cproduct[i + 1] if i < n - 1
+    @size[pos] %= M
     dfs2(u, pos)
   end
 end
