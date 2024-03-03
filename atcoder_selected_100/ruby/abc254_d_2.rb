dab64432f6ffa64bbd4ebf06344f91b3cebdd1b1
fumtas1k <fumta01a7@gmail.com>
2024-03-03 13:06:18 +0900

    [rename]変数名変更

--- atcoder_selected_100/ruby/abc254_d_2.rb
+++ atcoder_selected_100/ruby/abc254_d_2.rb
@@ -5,14 +5,14 @@
 
 N = gets.to_i
 
-num = [*0 .. N]
+nums = [*0 .. N]
 2.upto(Math.sqrt(N).to_i) do |i|
   i2 = i ** 2
   i2.step(N, i2) do |j|
-    while num[j] % i2 == 0
-      num[j] /= i2
+    while nums[j] % i2 == 0
+      nums[j] /= i2
     end
   end
 end
 
-puts num[1..].tally.map {|_, v| v ** 2 }.sum
+puts nums[1..].tally.map {|_, v| v ** 2 }.sum
