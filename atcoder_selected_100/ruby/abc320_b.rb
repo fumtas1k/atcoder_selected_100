dc1a89ff9a5241406f66a353444736f211da2a06
takasefumiyuki <71715982+fumtas1k@users.noreply.github.com>
2024-03-23 12:46:06 +0900

    abc320_b.rb の更新

--- atcoder_selected_100/ruby/abc320_b.rb
+++ atcoder_selected_100/ruby/abc320_b.rb
@@ -9,7 +9,7 @@
 def manacher(str)
   # 偶数長の場合も考慮し奇数長になるよう文字列に含まれない"$"を挿入
   s = str.chomp.chars.join("$").then { "$#{_1}$" }
-  # 中心を含む回文の半径
+  # 中心iの回文の半径
   radius = [0] * s.size
   i = 0
   loop do
