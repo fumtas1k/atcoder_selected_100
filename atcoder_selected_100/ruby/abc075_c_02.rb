417e4d48930f6afabac056499baaff100d009ae4
fumtas1k <fumta01a7@gmail.com>
2024-06-05 21:59:35 +0900

    [fix]不要な変数削除

--- atcoder_selected_100/ruby/abc075_c_02.rb
+++ atcoder_selected_100/ruby/abc075_c_02.rb
@@ -14,22 +14,20 @@ end
 
 ord = []
 low = []
-used = [false] * N
-idx = -1
-cnt = 0
+idx = cnt = 0
 
 dfs =-> (pos, pre = -1) do
-  used[pos] = true
-  ord[pos] = low[pos] = (idx += 1)
+  ord[pos] = low[pos] = idx
+  idx += 1
   G[pos].each do |to|
     next if to == pre
-    if used[to]
+    if ord[to]
       low[pos] = [low[pos], ord[to]].min
     else
       dfs.call(to, pos)
       low[pos] = [low[pos], low[to]].min
-      cnt += 1 if ord[pos] < low[to]
     end
+    cnt += 1 if ord[pos] < low[to]
   end
 end
 
