d6a216d477a5aa033aef97805b610b097a5922d9
fumtas1k <fumta01a7@gmail.com>
2024-09-11 22:35:47 +0900

    [fix]わかりやすいコードに修正

--- atcoder_selected_100/ruby/abc214_d.rb
+++ atcoder_selected_100/ruby/abc214_d.rb
@@ -6,16 +6,18 @@
 require "ac-library-rb/dsu"
 
 N = gets.to_i
-UVW = Array.new(N - 1) { gets.split.map(&:to_i) }
+UVW = Array.new(N - 1) { gets.split.map(&:to_i) }.sort_by(&:last)
 dsu = AcLibraryRb::DSU.new(N)
 
 # 主客転倒
 # 重みの小さい順に辺を追加していく
 # 小さい順に辺を追加していくと、その辺により連結された時の最大値はその辺の重みとなる
-ans = UVW.sort_by(&:last).sum do |u, v, w|
+ans = 0
+UVW.each do |u, v, w|
   u -= 1
   v -= 1
-  (dsu.size(u) * dsu.size(v) * w).tap { dsu.merge(u, v) }
+  ans += dsu.size(u) * dsu.size(v) * w
+  dsu.merge(u, v)
 end
 
 puts ans
