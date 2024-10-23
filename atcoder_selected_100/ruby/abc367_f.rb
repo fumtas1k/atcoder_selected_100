30f886297ba356cb18c8367caa511cf34b47aed2
fumtas1k <fumta01a7@gmail.com>
2024-10-23 21:35:15 +0900

    improve abc367f

--- atcoder_selected_100/ruby/abc367_f.rb
+++ atcoder_selected_100/ruby/abc367_f.rb
@@ -3,22 +3,22 @@
 # Zobrist Hash
 # Schwartz–Zippel lemma
 
-M = (1 << 61) - 1
+M = 2.pow(61) - 1
+
 N, Q = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
 B = gets.split.map(&:to_i)
 
 hash = (2 * 10 ** 5 + 1).times.map { rand(M) }
 
-acsum = A.reduce([0]) { |acc, i| acc << (acc[-1] + hash[i]) % M }
-bcsum = B.reduce([0]) { |acc, i| acc << (acc[-1] + hash[i]) % M }
+acsum = A.reduce([0]) { |acc, x| acc << (acc[-1] + hash[x]) % M }
+bcsum = B.reduce([0]) { |acc, x| acc << (acc[-1] + hash[x]) % M }
 
-ans = []
-Q.times do
+ans = Q.times.map do
   la, ra, lb, rb = gets.split.map(&:to_i)
   a = (acsum[ra] - acsum[la - 1]) % M
   b = (bcsum[rb] - bcsum[lb - 1]) % M
-  ans << (a == b ? "Yes" : "No")
+  a == b ? "Yes" : "No"
 end
 
 puts ans
