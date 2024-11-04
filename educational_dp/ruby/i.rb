9b31d962cd04957f07c1ba183d4e032e060beae9
fumtas1k <fumta01a7@gmail.com>
2024-11-04 21:48:49 +0900

    [fix]修正

--- educational_dp/ruby/i.rb
+++ educational_dp/ruby/i.rb
@@ -3,15 +3,15 @@
 N = gets.to_i
 P = gets.split.map(&:to_f)
 
-# dp[投げたコインの数][表が出たコインの数]
-dp = Array.new(N + 1) { [0] * (N + 1) }
+# dp[投げたコインの数][表が出たコインの数] = 確率
+dp = Array.new(N + 1) { Array.new((N + 1), 0) }
 dp[0][0] = 1
 
-N.times do |i|
-  0.upto(i) do |j|
-    dp[i + 1][j + 1] += dp[i][j] * P[i]
-    dp[i + 1][j] += dp[i][j] * (1 - P[i])
+P.each_with_index do |q, i|
+  (N + 1).times do |j|
+    dp[i + 1][j] += dp[i][j] * (1 - q)
+    dp[i + 1][j + 1] += dp[i][j] * q if j < N
   end
 end
 
-puts dp[-1][N / 2 + 1 ..].sum
+puts dp[-1][(N + 1) / 2 ..].sum
