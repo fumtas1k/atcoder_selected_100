faf8726d48ffb7e16e0b924eb49723825cf880a5
fumtas1k <fumta01a7@gmail.com>
2024-11-10 01:36:17 +0900

    [fix]説明追加

--- atcoder_selected_100/ruby/abc189_c.rb
+++ atcoder_selected_100/ruby/abc189_c.rb
@@ -3,7 +3,11 @@
 # ヒストグラム最大長方形
 
 N = gets.to_i
-A = gets.split.map(&:to_i) << 0
+A = gets.split.map(&:to_i)
+# 空にならないように先頭に最小値 を追加
+A.unshift(-1)
+# 最後の要素も取り出すために末尾に最小値 を追加
+A << -1
 
 stack = []
 max_area = 0
@@ -11,7 +15,8 @@ max_area = 0
 A.each_with_index do |a, i|
   while !stack.empty? && A[stack[-1]] > a
     height = A[stack.pop]
-    width = stack.empty? ? i : i - stack[-1] - 1
+    # 高さA[stack.pop]の長方形の幅は、stack[-1] + 1 から i - 1 まで
+    width = (i - 1) - stack[-1]
     max_area = [max_area, height * width].max
   end
   stack << i
