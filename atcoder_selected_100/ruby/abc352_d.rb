89fa0f02f502672b9fa27c864f8b7ef53af147a5
fumtas1k <fumta01a7@gmail.com>
2024-11-10 19:12:41 +0900

    [fix]シンプルに

--- atcoder_selected_100/ruby/abc352_d.rb
+++ atcoder_selected_100/ruby/abc352_d.rb
@@ -5,29 +5,26 @@
 
 class SlideWindow
   class << self
-    def min(window, data)
-      new(window, data, (window - 1).times.sort_by { data[_1] }, &:<)
-    end
+    def min(window, data) = new(window, data, &:<)
 
-    def max(window, data)
-      new(window, data, (window - 1).times.sort_by { -data[_1] }, &:>)
-    end
+    def max(window, data) = new(window, data, &:>)
   end
 
   attr_accessor :window, :data, :dequeue, :ope
 
-  def initialize(window, data, dequeue, &ope)
+  def initialize(window, data, &ope)
     @window = window
     @data = data
-    @dequeue = dequeue
+    @dequeue = []
     @ope = ope
   end
 
   def prod
-    (window - 1).upto(data.size - 1).each_with_object([]) do |i, res|
+    data.size.times.each_with_object([]) do |i, res|
       dequeue.shift while !dequeue.empty? && dequeue[0] < i - window + 1
       dequeue.pop while !dequeue.empty? && ope.(data[i], data[dequeue[-1]])
       dequeue << i
+      next if i < window - 1
       res << data[dequeue[0]]
     end
   end
@@ -41,4 +38,3 @@ smaller = SlideWindow.min(K, PINDEX).prod
 larger = SlideWindow.max(K, PINDEX).prod
 
 puts larger.zip(smaller).map { _1 - _2 }.min
-
