4409fee0277f03167298f8b8d323b58f1d22bb6d
fumtas1k <fumta01a7@gmail.com>
2024-11-16 23:57:28 +0900

    [fix]シンプルに

--- atcoder_selected_100/ruby/abc305_e.rb
+++ atcoder_selected_100/ruby/abc305_e.rb
@@ -1,5 +1,5 @@
 # -
-# ABC305/D
+# ABC305/E
 # 変形ダイクストラ法
 # 優先度付きキュー
 
@@ -15,9 +15,9 @@ end
 PH = Array.new(K) { gets.split.map(&:to_i).then { [_1[0].pred, _1[1]] } }
 
 def dijkstra
-  hitpoints = [- Float::INFINITY] * N
+  hitpoints = [-10 ** 18] * N
   PH.each {|pos, h| hitpoints[pos] = h }
-  log = AcLibraryRb::PriorityQueue.new(PH) {|u, v| u[1] > v[1] }
+  log = AcLibraryRb::PriorityQueue.new(PH) {|u, v| _1[1] > _2[1] }
   until log.empty?
     pos, hitpoint = log.pop
     next if hitpoints[pos] > hitpoint
@@ -31,5 +31,5 @@ def dijkstra
   hitpoints
 end
 
-ans = dijkstra.map.with_index {|hitpoint, i| hitpoint >= 0 ? i.succ : nil }.compact
+ans = dijkstra.filter_map.with_index { _2.succ if _1 >= 0 }
 puts [ans.size, ans.join(" ")]
