67f08735bbed3fcbc5367571ddbcb2f582a27ca9
fumtas1k <fumta01a7@gmail.com>
2024-12-01 11:58:32 +0900

    [fix]誤字修正と変数名修正

--- atcoder_selected_100/ruby/abc350_e.rb
+++ atcoder_selected_100/ruby/abc350_e.rb
@@ -5,16 +5,16 @@
 
 N, A, X, Y = gets.split.map(&:to_i)
 
-@memo = Hash.new(Float::INFINITY)
-@memo[0] = 0
+@dp = Hash.new(Float::INFINITY)
+@dp[0] = 0.0
 
-# dfs(n) = Y + 1 / 6 * dfs(n / 1) + 1 / 6 * dfs(n + 2) + ... + 1 / 6 * dfs(n / 6)
+# dfs(n) = Y + 1 / 6 * dfs(n / 1) + 1 / 6 * dfs(n / 2) + ... + 1 / 6 * dfs(n / 6)
 # -> dfs(n) = 6 / 5 * Y + 1 / 5 * dfs(n / 2) + ... + 1 / 5 * dfs(n / 6)
 def dfs(n)
-  return @memo[n] if @memo[n] != Float::INFINITY
+  return @dp[n] if @dp[n] != Float::INFINITY
   x = X + dfs(n / A)
   y = (6  * Y + (2 .. 6).sum { dfs(n / _1) }) / 5.to_r
-  @memo[n] = [x, y].min
+  @dp[n] = [x, y].min
 end
 
-puts dfs(N).to_f
+puts dfs(N)
