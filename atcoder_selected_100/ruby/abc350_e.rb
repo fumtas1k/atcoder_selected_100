d317f83cb507feb74e61814dc57f1e1abde7cecb
fumtas1k <fumta01a7@gmail.com>
2024-12-01 12:00:24 +0900

    [fix]不要な初期値削除

--- atcoder_selected_100/ruby/abc350_e.rb
+++ atcoder_selected_100/ruby/abc350_e.rb
@@ -5,13 +5,13 @@
 
 N, A, X, Y = gets.split.map(&:to_i)
 
-@dp = Hash.new(Float::INFINITY)
+@dp = Hash.new
 @dp[0] = 0.0
 
 # dfs(n) = Y + 1 / 6 * dfs(n / 1) + 1 / 6 * dfs(n / 2) + ... + 1 / 6 * dfs(n / 6)
 # -> dfs(n) = 6 / 5 * Y + 1 / 5 * dfs(n / 2) + ... + 1 / 5 * dfs(n / 6)
 def dfs(n)
-  return @dp[n] if @dp[n] != Float::INFINITY
+  return @dp[n] if @dp[n]
   x = X + dfs(n / A)
   y = (6  * Y + (2 .. 6).sum { dfs(n / _1) }) / 5.to_r
   @dp[n] = [x, y].min
