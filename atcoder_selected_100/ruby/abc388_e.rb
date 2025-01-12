f2e052ab4251daae0f853046b77a57d02d3d2b6f
fumtas1k <fumta01a7@gmail.com>
2025-01-12 09:45:51 +0900

    [fix]ngの値が大きくてもバグらないよう修正

--- atcoder_selected_100/ruby/abc388_e.rb
+++ atcoder_selected_100/ruby/abc388_e.rb
@@ -5,10 +5,10 @@
 N = gets.to_i
 A = gets.split.map(&:to_i)
 
-def double?(k) = k.times.all? { A[_1] * 2 <= A[N - k + _1] }
+def double?(k) = 2 * k <= N && k.times.all? { A[_1] * 2 <= A[N - k + _1] }
 
 ok = 0
-ng = N / 2 + 1
+ng = N
 while ng - ok > 1
   m = (ok + ng) / 2
   double?(m) ? ok = m : ng = m
