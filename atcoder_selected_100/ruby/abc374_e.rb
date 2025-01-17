2d9689e3b6e9f6c627fb0d7187839bcdd170bb5c
fumtas1k <fumta01a7@gmail.com>
2025-01-17 23:17:56 +0900

    [fix]解説修正

--- atcoder_selected_100/ruby/abc374_e.rb
+++ atcoder_selected_100/ruby/abc374_e.rb
@@ -3,25 +3,24 @@
 # 決め打ち二分探索
 # 最小値の最大化
 
+INF = 1 << 60
 N, X = gets.split.map(&:to_i)
 APBQ = Array.new(N) { gets.split.map(&:to_i) }
 
 def calc_x(w)
   APBQ.sum do |a, p, b, q|
     # コスパが良い方を機械Siにする
+    a, p, b, q = b, q, a, p if p / a.to_r > q / b.to_r
+
     # 機械SiをBi台 = 機械TiをAi台 = 処理能力AiBi個
-    # コスト最小化する場合、 w = AiBi * x + Ai * y　+ Bi * z = Ai * (Bi * x + y) + Bi * z
-    # Siを(Bi * x + y)台、Tiをz台購入すれば良い。
-    # zはAi未満(TiをAi台以上購入するくらいなら、Siを購入した方が安い)
-    if p / a.to_r > q / b.to_r
-      a, b, p, q = b, a, q, p
+    # コスト最小化する場合、 w <= Ai * Bi * x + Ai * y　+ Bi * z = Ai * (Bi * x + y) + Bi * z = Ai * j + Bi * k
+    # Siをj台、Tiをk台購入すれば良い。
+    # kはAi未満(TiをAi台以上購入するくらいなら、Siを購入した方が安い)
+    a.times.reduce(INF) do |acc, k|
+      # 切り上げるために a - 1 を足している
+      j = [(w - b * k + (a - 1)) / a, 0].max
+      [acc, p * j + q * k].min
     end
-    res = Float::INFINITY
-    a.times do |j|
-      i = [(w - b * j + a - 1) / a, 0].max
-      res = [res, p * i + q * j].min
-    end
-    res
   end
 end
 
