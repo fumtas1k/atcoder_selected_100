c20a3555ea17e6b865afba2a2be12e0d4e9202de
fumtas1k <fumta01a7@gmail.com>
2025-03-26 21:23:40 +0900

    [fix]無駄な記述削除

--- atcoder_selected_100/ruby/abc302_e.rb
+++ atcoder_selected_100/ruby/abc302_e.rb
@@ -13,16 +13,16 @@ cnt = N
 Q.times do
   case gets.split.map(&:to_i).map(&:pred)
   in [0, u, v]
-    cnt -= 1 if connections[u].size.zero?
-    cnt -= 1 if connections[v].size.zero?
+    cnt -= 1 if connections[u].empty?
+    cnt -= 1 if connections[v].empty?
     connections[u] << v
     connections[v] << u
   in [1, u]
     connections[u].each do |v|
       connections[v].delete(u)
-      cnt += 1 if connections[v].size.zero?
+      cnt += 1 if connections[v].empty?
     end
-    cnt += 1 if connections[u].size > 0
+    cnt += 1 unless connections[u].empty?
     connections[u].clear
   end
   ans << cnt
