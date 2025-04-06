eed3d29f02ac06d1b5c336f453a46d413a453004
fumtas1k <fumta01a7@gmail.com>
2025-04-06 12:47:33 +0900

    [fix]説明追加とスマートに修正

--- atcoder_selected_100/ruby/abc254_d_2.rb
+++ atcoder_selected_100/ruby/abc254_d_2.rb
@@ -3,16 +3,19 @@
 # 素因数分解
 # 平方数
 
+# i * j が平方数
+# i の約数のうち最大の平方数で i を割った値が a、 j　の約数のうち最大の平方数で j を割った値が b
+# a == b が成り立つ
+# つまり, aの個数を求めれば良い
 N = gets.to_i
 
-nums = [*0 .. N]
-2.upto(Math.sqrt(N).to_i) do |i|
+factors = Array.new(N + 1, &:itself)
+2.upto(N) do |i|
   i2 = i ** 2
-  i2.step(N, i2) do |j|
-    while nums[j] % i2 == 0
-      nums[j] /= i2
-    end
-  end
+  # Math.sqrtは丸誤差があるので、条件指定のbreakを使用する
+  break if i2 > N
+  x.step(N, i2) { factors[_1] /= i2 while factors[_1] % i2 == 0 }
 end
 
-puts nums[1..].tally.map {|_, v| v ** 2 }.sum
+# 0を除く
+puts factors[1..].tally.sum { _2 ** 2 }
--- atcoder_selected_100/ruby/abc346_e.rb
+++ atcoder_selected_100/ruby/abc346_e.rb
@@ -3,30 +3,33 @@
 # 数学的問題
 # 後ろから考える
 
-require "set"
-
 H, W, M = gets.split.map(&:to_i)
-TAX = Array.new(M) { gets.split.map(&:to_i) }
-ROW = Set.new
-COLUMN = Set.new
+rows = Hash.new
+cols = Hash.new
 
-ans = Hash.new(0)
-# 最後に塗られたマスの色は確定
-# 最後から逆順に見てすでに塗られているマスは固定
+TAX = Array.new(M) { gets.split.map(&:to_i) }
+
+cnts = Hash.new(0)
+cnts[0] = H * W
 TAX.reverse.each do |t, a, x|
+  cnt = 0
   a -= 1
   case t
   when 1
-    next unless ROW.add?(a)
-    ans[x] += W - COLUMN.size unless x.zero?
+    next if rows[a]
+    rows[a] = x
+    cnt = W - cols.size
+    cnts[x] += cnt
   when 2
-    next unless COLUMN.add?(a)
-    ans[x] += H - ROW.size unless x.zero?
-  else
+    next if cols[a]
+    cols[a] = x
+    cnt = H - rows.size
+    cnts[x] += cnt
   end
+  cnts[0] -= cnt
 end
 
-ans[0] = H * W - ans.values.sum
-ans.reject! {|_, v| v.zero? }
+
+ans = cnts.sort.filter_map { _1.join(" ") unless _1[1].zero? }
 puts ans.size
-puts ans.sort.map { _1.join(" ") }.join("\n")
+puts ans
