fc880935984a02c14fd18183640202bf3de1e86e
fumtas1k <fumta01a7@gmail.com>
2025-04-20 09:51:56 +0900

    [fix]解説回答に合わせて修正

--- atcoder_selected_100/ruby/abc402_e.rb
+++ atcoder_selected_100/ruby/abc402_e.rb
@@ -6,20 +6,18 @@
 N, X = gets.split.map(&:to_i)
 SCP = Array.new(N) { gets.split.map(&:to_i) }
 
-# dp[x][bit] = x 円で 正答が　bit　の状態の時の期待値の最大値
+# dp[x][bit] = x円払って 正解が bit の状態の場合の最大期待値
 dp = Array.new(X + 1) { Array.new(1 << N, 0) }
 
-ans = 0
-X.downto(0) do |x|
+(X + 1).times do |x|
   (1 << N).times do |bit|
     N.times do |i|
-      next if bit[i].zero?
+      next if bit[i] == 1
       s, c, p = SCP[i]
-      next if x + c > X
-      dp[x][bit] = [dp[x][bit], dp[x + c][bit] * (100 - p) / 100.to_f + (dp[x + c][bit ^ (1 << i)] + s) * p / 100.to_f].max
-      ans = [ans, dp[x][bit]].max
+      next if x - c < 0
+      dp[x][bit] = [dp[x][bit], dp[x - c][bit] * (100 - p) / 100.to_f + (dp[x - c][bit | (1 << i)] + s) * p / 100.to_f].max
     end
   end
 end
 
-puts ans
+puts dp[X].max
