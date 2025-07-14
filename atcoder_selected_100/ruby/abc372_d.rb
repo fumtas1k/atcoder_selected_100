4fb1efa6dbbd9af362667d6fa89bbe71a8c9c36e
fumtas1k <fumta01a7@gmail.com>
2025-07-14 23:36:16 +0900

    スタックの維持に関するコメントを追加

--- atcoder_selected_100/ruby/abc372_d.rb
+++ atcoder_selected_100/ruby/abc372_d.rb
@@ -1,6 +1,7 @@
 # -
 # ABC372/D
 # スタック
+# 単調減少
 
 N = gets.to_i
 H = gets.split.map(&:to_i)
@@ -8,6 +9,7 @@ H = gets.split.map(&:to_i)
 ans = Array.new(N, 0)
 stack = []
 (N - 2).downto(0) do |i|
+  # H[i+1]を含めた単調減少スタックを維持
   stack.pop while !stack.empty? && stack[-1] < H[i + 1]
   stack << H[i + 1]
   ans[i] = stack.size
