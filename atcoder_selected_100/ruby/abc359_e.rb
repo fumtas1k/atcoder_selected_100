d6198fcb890930ba5afd885e13151314252def7f
fumtas1k <fumta01a7@gmail.com>
2025-07-16 21:44:28 +0900

    ABC359/Eの水槽の水の量の記録方法を修正し、コードを簡素化しました

--- atcoder_selected_100/ruby/abc359_e.rb
+++ atcoder_selected_100/ruby/abc359_e.rb
@@ -6,25 +6,19 @@
 N = gets.to_i
 H = gets.split.map(&:to_i)
 
-# 水槽の水の量(高さ、幅）を記録する。
-# [height, width]
-stack = [[0, 0]]
+# 水槽の水の量(高さ）を記録する。
+# [index, height]
+stack = [[0, 10 ** 9 + 1]]
 ans = Array.new(N + 1, 0)
 
-H.each_with_index do |h, i|
-  ans[i + 1] = ans[i]
-  w = 1
-  while !stack.empty? && stack[-1][0] < h
-    height, width = stack.pop
-    w += width
-    # 直近でh以上の高さの板との間をhの高さまで水で満たすため
-    # すでに満たされている水を満たすための時間を引く
-    ans[i + 1] -= height * width
-  end
-  stack << [h, w]
-  ans[i + 1] += h * w
+H.each.with_index(1) do |h, i|
+  stack.pop while !stack.empty? && stack[-1][1] < h
+  j = stack[-1][0]
+  # jまではすでに水が満たされているので、ans[j] を足す
+  ans[i] = (i - j) * h + ans[j]
+  stack << [i, h]
 end
 
 # ans[i] = i番目の板の手前までを満たす時刻
 # したがって、i番目の水槽に水が到達するのは ans[i] + 1
-puts ans[1..].map(&:succ).join(" ")
+puts ans[1..].map(&:succ).join(' ')
