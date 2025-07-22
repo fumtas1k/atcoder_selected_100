20e7074bbd382d57c2c4ab7978d53f599ac50c5d
fumtas1k <fumta01a7@gmail.com>
2025-07-22 23:44:58 +0900

    入力の取得方法を修正し、コードの可読性を向上させました

--- atcoder_selected_100/ruby/abc230_d.rb
+++ atcoder_selected_100/ruby/abc230_d.rb
@@ -2,13 +2,12 @@
 # 貪欲法
 # 区間スケジューリング問題
 
-N, D = gets.split.map(&:to_i)
+N, D = gets.chomp.split.map(&:to_i)
 LR = Array.new(N) { gets.split.map(&:to_i) }.sort_by(&:last)
 
-ans = 0
-j = -1
-LR.each_with_index do |(l, r), i|
-  next if j >= l
+ans = j = 0
+LR.each do |l, r|
+  next if l <= j
   j = r + D - 1
   ans += 1
 end
