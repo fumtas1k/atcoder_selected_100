99f0099eaf56c5378c408c6fa3a85d957a168542
fumtas1k <fumta01a7@gmail.com>
2025-10-12 01:05:35 +0900

    dpのコメントを修正し、手番の勝ち条件を明確化

--- atcoder_selected_100/ruby/abc427_d.rb
+++ atcoder_selected_100/ruby/abc427_d.rb
@@ -13,20 +13,20 @@ T = gets.to_i.times do
     rev_edges[v] << u
   end
 
-  # dp[i] = Aliceが勝つ場合をtrue, Bobが勝つ場合をfalseとする
-  # 後ろから考える
+  # dp[i] = 手番が勝つ場合true
+  # 後ろから考えると最後の手番はAlice
   dp = Array.new(n) { ss[_1] == "A" }
-  (2 * k - 1).downto(0) do |i|
-    # 偶数がAliceのターン, 奇数がBobのターン
-    # そのターンの人が負ける値を初期値としておく
-    ep = Array.new(n, i.odd?)
+  (2 * k).times do |i|
+    ep = [false] * n
     dp.each_with_index do |bool, to|
-      next if bool == i.odd?
+      # bool = false の場所に置けるなら手番の勝ち
+      next if bool
       rev_edges[to].each do |from|
-        ep[from] = bool
+        ep[from] = true
       end
     end
     dp = ep
   end
+  # 最初の手番はAlice
   puts dp[0] ? "Alice" : "Bob"
 end
