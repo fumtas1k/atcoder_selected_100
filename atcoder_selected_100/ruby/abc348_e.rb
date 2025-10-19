0493a1f56f272f0b845ef6ace364195f2e154310
fumtas1k <fumta01a7@gmail.com>
2025-10-19 18:46:01 +0900

    abc348/eの重心計算に関するコメントを追加し、変数名を修正

--- atcoder_selected_100/ruby/abc348_e.rb
+++ atcoder_selected_100/ruby/abc348_e.rb
@@ -3,6 +3,7 @@
 # 深さ優先探索
 # 木の重心
 
+# sum(Ci * d(x, i))が最小となるxは重心である
 N = gets.to_i
 G = Array.new(N) { [] }
 (N - 1).times do
@@ -14,12 +15,13 @@ C = gets.split.map(&:to_i)
 
 @total = C.sum
 @center = -1
+# 重心を求める
 def dfs1(pos = 0, pre = -1)
   res = C[pos]
   max = 0
-  G[pos].each do |i|
-    next if i == pre
-    now = dfs1(i, pos)
+  G[pos].each do |nex|
+    next if nex == pre
+    now = dfs1(nex, pos)
     max = [max, now].max
     res += now
   end
@@ -27,12 +29,13 @@ def dfs1(pos = 0, pre = -1)
   max = [max, @total - res].max
   # 部分木の重みの合計の最大値がそれ以外より大きければそちらに中心を移動した方が Ci * d(x, i)の合計が最小化する。
   # つまりそちらの方が木の重心になる
-  # max <= @total - max であるなら重心。最大で２カ所存在するがどちらでも問題ない
+  # max <= @total / 2 であるなら重心。最大で2カ所存在するがどちらでも問題ない
   @center = pos if max * 2 <= @total
   res
 end
 
 @ans = 0
+# sum(Ci * d(pos, i))を求める
 def dfs2(pos = @center, pre = -1, dist = 0)
   @ans += dist * C[pos]
   G[pos].each do |i|
