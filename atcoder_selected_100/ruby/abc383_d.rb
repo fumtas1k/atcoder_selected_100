e812e4f1ae55e17f3defbf9486c50da3192cee9f
fumtas1k <fumta01a7@gmail.com>
2025-11-09 13:52:51 +0900

    コードの可読性を向上させるために、変数名を修正し、コメントを追加しました。また、素数のリスト生成と約数計算のロジックを最適化しました。

--- atcoder_selected_100/ruby/abc383_d.rb
+++ atcoder_selected_100/ruby/abc383_d.rb
@@ -8,28 +8,33 @@
 
 # 整数Mは、素数p1, p2, ... pk を用いて p1^e1 * p2^e2 * ... * pk^ek と表すことができる。
 # このとき、Mの約数の個数は (e1 + 1) * (e2 + 1) * ... * (ek + 1) である。
+# 正の約数が9個ということは、以下の2通りの場合がある。
+# 1. ある素因数の指数が8で、他の素因数の指数が0である場合 (e1 = 8)
+# 2. ある2つの素因数の指数が2で、他の素因数の指数が0である場合 (e1 = 2, e2 = 2)
 
 N = gets.to_i
-LIMIT = Math.sqrt(N).to_i + 1
+sqrt = Math.sqrt(N).to_i + 1
+sqrt -= 1 while sqrt.pow(2) > N
 
 primes = []
-is_prime = Array.new(LIMIT + 1, true)
-2.upto(LIMIT) do |i|
+is_prime = [true] * (sqrt + 1)
+is_prime[0] = is_prime[1] = false
+2.upto(sqrt) do |i|
   next unless is_prime[i]
   primes << i
-  (i * 2).step(LIMIT, i) do |j|
-    is_prime[j] = false
-  end
+  (i * 2).step(sqrt, i) { is_prime[_1] = false }
 end
 
 ans = 0
+
+l = 0
 r = primes.size - 1
-primes.each_with_index do |prime, l|
-  break if prime.pow(4) > N
-  ans += 1 if prime.pow(8) <= N
-  r -= 1 while r > 0 && prime.pow(2) * primes[r].pow(2) > N
-  next if r < l
+while l < r
+  p1 = primes[l]
+  ans += 1 if p1.pow(8) <= N
+  r -= 1 while l < r && p1.pow(2) * primes[r].pow(2) > N
   ans += r - l
+  l += 1
 end
 
 puts ans
