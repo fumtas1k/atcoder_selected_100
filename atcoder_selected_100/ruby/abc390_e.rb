70bd5c42e3ecc6c0cfbae637d9f13c7488c7c87b
fumtas1k <fumta01a7@gmail.com>
2025-11-10 21:18:27 +0900

    bsearch関数を定義し、コードの可読性を向上させるために変数名を修正しました。また、単調増加の補正ロジックを整理しました。

--- atcoder_selected_100/ruby/abc390_e.rb
+++ atcoder_selected_100/ruby/abc390_e.rb
@@ -3,40 +3,41 @@
 # 決め打ち二分探索, 最小値の最大化
 
 INF = 1 << 60
+def bsearch(ok, ng, &block)
+  while (ng - ok).abs > 1
+    mid = (ok + ng) / 2
+    block.(mid) ? ok = mid : ng = mid
+  end
+  ok
+end
 
 N, X = gets.split.map(&:to_i)
-dp = Array.new(3) { Array.new(X + 1, -INF) }
+# dp[i][x] := カロリーの合計がxのときのビタミンViの摂取量の最大値
+dp = Array.new(3) { [-INF] * (X + 1) }
 3.times { dp[_1][0] = 0 }
 
 N.times do
   v, a, c = gets.split.map(&:to_i)
   v -= 1
-  X.downto(c) do |j|
-    dp[v][j] = [dp[v][j], dp[v][j - c] + a].max
+  X.downto(c) do |i|
+    dp[v][i] = [dp[v][i], dp[v][i - c] + a].max
   end
 end
 
-# 単調増加にする
-3.times do |v|
-  X.times do |j|
-    dp[v][j + 1] = [dp[v][j], dp[v][j + 1]].max
+# 単調増加になるよう補正
+3.times do |i|
+  X.times do |x|
+    dp[i][x + 1] = [dp[i][x + 1], dp[i][x]].max
   end
 end
 
-judge = ->(a) do
+can_min = -> a do
   x = 0
-  3.times do |v|
-    return false if dp[v][X] < a
-    x += (0 .. X).bsearch { dp[v][_1] >= a }
+  3.times do |i|
+    return false if dp[i][X] < a
+    x += bsearch(X, 0) { dp[i][_1] >= a }
   end
   x <= X
 end
 
-ok = 0
-ng = 10 ** 9 + 1
-while ng - ok > 1
-  mid = (ok + ng) / 2
-  judge.(mid) ? ok = mid : ng = mid
-end
-
-puts ok
+puts bsearch(0, 10 ** 9 + 1, &can_min)
