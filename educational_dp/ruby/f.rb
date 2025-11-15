cbb0b8945abaafe80e9af8163ee46b20d11db6a8
fumtas1k <fumta01a7@gmail.com>
2025-11-16 01:35:40 +0900

    LCS探索のロジックを可読性の高い形式に変更し、変数名を見直しました。また、復元処理の構造を整理しました。

--- educational_dp/ruby/f.rb
+++ educational_dp/ruby/f.rb
@@ -2,30 +2,30 @@
 
 S = gets.chomp.chars
 T = gets.chomp.chars
-
 dp = Array.new(S.size + 1) { [0] * (T.size + 1) }
 
 # LCSの探索
-S.size.times do |i|
-  T.size.times do |j|
+S.each_with_index do |s, i|
+  T.each_with_index do |t, j|
     dp[i + 1][j + 1] = [dp[i + 1][j], dp[i][j + 1]].max
-    dp[i + 1][j + 1] = [dp[i + 1][j + 1], dp[i][j] + 1].max if S[i] == T[j]
+    dp[i + 1][j + 1] = [dp[i + 1][j + 1], dp[i][j] + 1].max if s == t
   end
 end
 
 # 復元
-i = S.size
-j = T.size
+s = S.size
+t = T.size
 ans = ""
-while i > 0 && j > 0
-  if dp[i][j] == dp[i - 1][j]
-    i -= 1
-  elsif dp[i][j] == dp[i][j - 1]
-    j -= 1
-  else # dp[i][j] == dp[i - 1][j - 1] + 1
-    i -= 1
-    j -= 1
-    ans += S[i]
+while s > 0 && t > 0
+  case dp[s][t]
+  when dp[s - 1][t]
+    s -= 1
+  when dp[s][t - 1]
+    t -= 1
+  else dp[s - 1][t - 1]
+    s -= 1
+    t -= 1
+    ans << S[s]
   end
 end
 
