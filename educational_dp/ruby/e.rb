3033f45e77f7a4cea4aab92d2da29322a8c7cf36
fumtas1k <fumta01a7@gmail.com>
2025-11-16 00:45:42 +0900

    dp配列の初期化方法を修正し、重さと価値の処理ロジックを整理しました。可読性を向上させるために、変数名を見直しました。

--- educational_dp/ruby/e.rb
+++ educational_dp/ruby/e.rb
@@ -1,20 +1,19 @@
 # E
 
-# 重さの上限が10の6乗を超えており、価値の上限が10の6乗以下なので通常とは逆のやり方をする
-V_MAX = 10 ** 3 * 10 ** 2
-N, W = gets.split.map(&:to_i)
+INF = 1 << 60
 
-# indexが重さ、値が価値
-dp = [Float::INFINITY] * (V_MAX + 1)
+N, W = gets.split.map(&:to_i)
+WV = Array.new(N) { gets.split.map(&:to_i) }
+V_MAX = N * 10 ** 3
+
+# dp[i] = 価値iのときの重さの総和の最小値
+dp = [INF] * (V_MAX + 1)
 dp[0] = 0
 
-N.times do |i|
-  w, v = gets.split.map(&:to_i)
-  (V_MAX).downto(v) do |j|
-    # 同じ価値なら重さの合計が小さい方がより価値を高められるため重さを更新
-    dp[j] = [dp[j], dp[j - v] + w].min
+WV.each do |w, v|
+  V_MAX.downto(v) do |i|
+    dp[i] = [dp[i], dp[i - v] + w].min
   end
 end
 
-# 重さがW以下で価値が最大のものを求める
-puts (0 .. V_MAX).filter { dp[_1] <= W }.max
+puts V_MAX.downto(0).find { dp[_1] <= W }
