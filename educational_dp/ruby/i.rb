d0e9f1588a40a606d235c723535c6fda1ec452b0
fumtas1k <fumta01a7@gmail.com>
2025-11-16 10:14:58 +0900

    二次元配列から一次元配列に変更し、コインの表が出る確率の計算ロジックを簡素化しました。可読性を向上させるために、変数名を見直しました。

--- educational_dp/ruby/i.rb
+++ educational_dp/ruby/i.rb
@@ -3,15 +3,15 @@
 N = gets.to_i
 P = gets.split.map(&:to_f)
 
-# dp[投げたコインの数][表が出たコインの数] = 確率
-dp = Array.new(N + 1) { Array.new((N + 1), 0) }
-dp[0][0] = 1
+# dp[i] = 表が出たコインの個数がiである確率
+dp = [0] * (N + 1)
+dp[0] = 1
 
-P.each_with_index do |q, i|
-  (N + 1).times do |j|
-    dp[i + 1][j] += dp[i][j] * (1 - q)
-    dp[i + 1][j + 1] += dp[i][j] * q if j < N
+P.each do |p|
+  N.downto(0) do |i|
+    dp[i] = dp[i] * (1 - p)
+    dp[i] += dp[i - 1] * p if i >= 1
   end
 end
 
-puts dp[-1][(N + 1) / 2 ..].sum
+puts dp[(N + 1) / 2 ..].sum
