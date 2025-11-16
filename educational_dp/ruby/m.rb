75d411348aaae6e275809e9450500921be69707a
fumtas1k <fumta01a7@gmail.com>
2025-11-16 13:26:22 +0900

    累積和の計算方法を改善し、DP配列の初期化を修正しました。可読性を向上させるために、コメントを追加しました。

--- educational_dp/ruby/m.rb
+++ educational_dp/ruby/m.rb
@@ -1,20 +1,22 @@
 # M
+# 累積和
 
 M = 10 ** 9 + 7
 N, K = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
 
-# 貰うDP [i人目までの子][分け合う飴の個数] = 通り数
-dp = Array.new(N + 1) { [0] * (K + 1) }
-dp[0][0] = 1
+# dp[i] := 分け合う飴の数がiのときの場合の数
+dp = [0] * (K + 1)
+dp[0] = 1
 
-A.each_with_index do |a, i|
-  # dp[i]の累積和を求める
-  csum = dp[i].reduce([0]) { |acc, j| acc << acc[-1] + j }
-
-  (K + 1).times do |j|
-    dp[i + 1][j] = (csum[j + 1] - csum[[j - a, 0].max]) % M
+A.each do |a|
+  ep = []
+  csum = [0]
+  (K + 1).times do |i|
+    csum[i + 1] = (csum[i] + dp[i]) % M
+    ep[i] = (csum[i + 1] - csum[[i - a, 0].max]) % M
   end
+  dp = ep
 end
 
-puts dp[-1][-1]
+puts dp[K]
