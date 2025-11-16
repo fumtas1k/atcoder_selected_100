4c6c5be975487d4dc2be0944e5cd4fe98a93a8a4
fumtas1k <fumta01a7@gmail.com>
2025-11-16 11:14:57 +0900

    配列の初期化方法を修正し、変数名を見直しました。また、DFSロジックの可読性を向上させるために、期待値の計算を修正しました。

--- educational_dp/ruby/j.rb
+++ educational_dp/ruby/j.rb
@@ -1,11 +1,11 @@
 # J
 
 N = gets.to_i
-A = gets.split.map(&:to_i).tally
-A.default = 0
+A = gets.split.map(&:to_i)
+tally = A.tally
+tally.default = 0
 
-# DP[i][j][k] := 3個の皿の数i, 2個の皿の数j, 1個の皿の数k, 0個の皿の数N-i-j-kの時の操作回数の期待値
-# 求める値はDP[A[3]][A[2]][A[1]]
+# DP[i][j][k] = 残り1皿がi枚、2皿がj枚、3皿がk枚のときの操作回数の期待値
 # DP[i][j][k] = (DP[i][j][k]*(N-i-j-k)/N + DP[i-1][j+1][k]*i/N + DP[i][j-1][k+1]*j/N + DP[i][j][k-1]*k/N) + 1
 # 変形すると
 # DP[i][j][k] = (DP[i-1][j+1][k] + DP[i][j-1][k+1] + DP[i][j][k-1] + N) / (i+j+k)
@@ -14,11 +14,11 @@ DP[0][0][0] = 0.0
 
 def dfs(i, j, k)
   return DP[i][j][k] if DP[i][j][k]
-  exp = N
-  exp += dfs(i - 1, j + 1, k) * i if i > 0
-  exp += dfs(i, j - 1, k + 1) * j if j > 0
-  exp += dfs(i, j, k - 1) * k if k > 0
+  exp = N.to_f
+  exp += dfs(i - 1, j, k) * i if i >= 1
+  exp += dfs(i + 1, j - 1, k) * j if j >= 1
+  exp += dfs(i, j + 1, k - 1) * k if k >= 1
   DP[i][j][k] = exp / (i + j + k)
 end
 
-puts dfs(A[3], A[2], A[1])
+puts dfs(tally[1], tally[2], tally[3])
