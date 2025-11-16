d4bfcd4b8f0847cb224b886d38b7c74fab79a8ce
fumtas1k <fumta01a7@gmail.com>
2025-11-16 09:47:59 +0900

    定数名を修正し、配列の初期化方法を変更しました。また、動的計画法のロジックを整理し、可読性を向上させました。

--- educational_dp/ruby/h.rb
+++ educational_dp/ruby/h.rb
@@ -1,18 +1,20 @@
 # H
 
-M = 10 ** 9 + 7
+MOD = 10 ** 9 + 7
+HASH, DOT = "#.".bytes
 H, W = gets.split.map(&:to_i)
-A = Array.new(H) { gets.chomp.chars }
-
+A = Array.new(H) { gets.chomp.bytes }
 dp = Array.new(H) { [0] * W }
 dp[0][0] = 1
 
-H.times do |i|
-  W.times do |j|
-    next if A[i][j] == "#"
-    dp[i][j] += dp[i - 1][j] if i >= 1
-    dp[i][j] += dp[i][j - 1] if j >= 1
-    dp[i][j] %= M
+H.times do |r|
+  W.times do |c|
+    next if dp[r][c].zero? || A[r][c] == HASH
+    [[1, 0], [0, 1]].each do |dr, dc|
+      nr, nc = r + dr, c + dc
+      next unless nr <= H && nc < W && A[nr][nc] == DOT
+      dp[nr][nc] = (dp[nr][nc] + dp[r][c]) % MOD
+    end
   end
 end
 
