7f223abb1fbd9ad25bcf8c1644fba7c1a0187a8c
fumtas1k <fumta01a7@gmail.com>
2025-11-16 11:33:33 +0900

    dpロジックの可読性を向上させるために、ループ構造を修正しました。また、配列の初期化方法を整理しました。

--- educational_dp/ruby/k.rb
+++ educational_dp/ruby/k.rb
@@ -1,16 +1,15 @@
 # K
 
 N, K = gets.split.map(&:to_i)
-A = gets.split.map(&:to_i)
+A = gets.split.map(&:to_i).sort
 
 # dp[i]とはi個の石が残っている場合に先手が勝つときtrue
 dp = [false] * (K + 1)
-A[0].upto(K) do |i|
+
+(K + 1).times do |i|
   A.each do |j|
-    if i >= j && !dp[i - j]
-      dp[i] = true
-      break
-    end
+    break if i < j || dp[i]
+    dp[i] |= !dp[i - j]
   end
 end
 
