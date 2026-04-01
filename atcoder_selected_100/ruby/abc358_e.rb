0d74c9be960dea46c3f2c753c66be9d361a8b0d3
fumtas1k <fumta01a7@gmail.com>
2026-04-01 22:12:05 +0900

    refactor

--- atcoder_selected_100/ruby/abc358_e.rb
+++ atcoder_selected_100/ruby/abc358_e.rb
@@ -5,20 +5,24 @@
 # 逆元
 
 M = 998244353
-FACT = [1]
-FACT_INV = [1]
-1.upto(1000) do |i|
-  FACT[i] = FACT[-1] * i % M
-  FACT_INV[i] = FACT_INV[-1] * i.pow(M - 2, M) % M
-end
-
-def ncr(n, r)
-  (FACT[n] * FACT_INV[n - r] % M) * FACT_INV[r] % M
-end
 
 K = gets.to_i
 # 0個の英大文字は不要なので除去
-C = gets.split.map(&:to_i).filter { !_1.zero? }
+C = gets.split.map(&:to_i).filter(&:positive?)
+
+fact = [1]
+fact_inv = []
+1.upto(K) do |i|
+  fact[i] = fact[-1] * i % M
+end
+fact_inv[K] = fact[K].pow(M - 2, M)
+K.downto(1) do |i|
+  fact_inv[i - 1] = fact_inv[i] * i % M
+end
+
+ncr = ->(n, r) do
+  (fact[n] * fact_inv[n - r] % M) * fact_inv[r] % M
+end
 
 # dp[i:=文字種][j:=文字数] = 場合の数
 dp = Array.new(C.size + 1) { Array.new(K + 1, 0) }
@@ -29,7 +33,7 @@ C.size.times do |i|
     ([C[i], K - j].min + 1).times do |k|
       # すでに置かれている文字列の配置は無視し、新たな文字種k個の配置を考える。
       # j+k個の文字列で、同じ文字k個の配置の数は j+kCk
-      dp[i + 1][j + k] += dp[i][j] * ncr(j + k, k)
+      dp[i + 1][j + k] += dp[i][j] * ncr.(j + k, k)
       dp[i + 1][j + k] %= M
     end
   end
